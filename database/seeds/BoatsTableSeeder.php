444607ad755e467910cd845ac11372ff4ac9db5e
Thijs Vunderink <thijsvunderink@gmail.com>
2018-04-18 17:37:16 +0200

    [W1-1] Seeders boats and users

--- database/seeds/BoatsTableSeeder.php
+++ database/seeds/BoatsTableSeeder.php
@@ -20,8 +20,6 @@ class BoatsTableSeeder extends Seeder
       'user_id' => '2',
       'created_at' => '2018-01-01 10:00:00',
       'updated_at' => '2018-01-01 10:00:00',
-      'date_available' => '31-03-2018',
-      'description' => 'Beschrijving bij deze kamer. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.'
       ]);
 
 
@@ -34,8 +32,6 @@ class BoatsTableSeeder extends Seeder
       'user_id' => '2',
       'created_at' => '2018-01-01 10:10:00',
       'updated_at' => '2018-01-01 10:10:00',
-      'date_available' => '31-03-2018',
-      'description' => 'Beschrijving bij deze kamer. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.'
       ]);
 
 
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,21 +13,18 @@ class UsersTableSeeder extends Seeder
     {
        DB::table('users')->insert([
            'name' => 'admin',
-           'role' => 'Administrator',
            'email' => 'admin@admin.com',
            'password' => bcrypt('admin'),
        ]);
 
        DB::table('users')->insert([
            'name' => 'TestVerhuurder',
-           'role' => 'Verhuurder',
            'email' => 'verhuur@test.com',
            'password' => bcrypt('verhuurder'),
        ]);
 
        DB::table('users')->insert([
            'name' => 'TestHuurder',
-           'role' => 'Huurder',
            'email' => 'huur@test.com',
            'password' => bcrypt('huurder'),
        ]);
